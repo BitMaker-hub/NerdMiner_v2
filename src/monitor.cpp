1703e3260eb317b1691f30176566e63f5ece8121
BitMaker <bitmaker.me@proton.me>
2023-09-09 00:07:07 +0200

    Minimal changes

--- src/monitor.cpp
+++ src/monitor.cpp
@@ -188,7 +188,7 @@ String getTime(void){
   //Check if need an NTP call to check current time
   if((mTriggerUpdate == 0) || (millis() - mTriggerUpdate > UPDATE_PERIOD_h * 60 * 60 * 1000)){ //60 sec. * 60 min * 1000ms
     if(WiFi.status() == WL_CONNECTED) {
-        if(timeClient.update()) mTriggerUpdate = millis();
+         if(timeClient.update()) mTriggerUpdate = millis();
         initialTime = timeClient.getEpochTime(); // Guarda la hora inicial (en segundos desde 1970)
         Serial.print("TimeClient NTPupdateTime ");
     }
