BitMaker-hub/NerdMiner_v2

commit 25db0a45a6adf0d6202537819d4e0b7b9cda1724

bin files updated and added sprite destroy on Tdisplav1

src/drivers/displays/tDisplayV1Driver.cpp
@@ -96,6 +96,7 @@ void tDisplay_MinerScreen(unsigned long mElapsed)
 
   // Push prepared background to screen
   background.pushSprite(0, 0);
+  background.deleteSprite();
 }
 
 void tDisplay_ClockScreen(unsigned long mElapsed)
@@ -134,6 +135,7 @@ void tDisplay_ClockScreen(unsigned long mElapsed)
 
   // Push prepared background to screen
   background.pushSprite(0, 0);
+  background.deleteSprite();
 }
 
 void tDisplay_GlobalHashScreen(unsigned long mElapsed)
@@ -193,6 +195,7 @@ void tDisplay_GlobalHashScreen(unsigned long mElapsed)
 
   // Push prepared background to screen
   background.pushSprite(0, 0);
+  background.deleteSprite();
 }
 
 void tDisplay_LoadingScreen(void)

commit 29ffe62ba875e119662bbea23a76bb7d0a41e585

fix regression introduced in 25db0a4 that prevented T-Display_V1 display to updates; fix for displayDriver.h case sensitive filename

src/drivers/displays/amoledDisplayDriver.cpp
@@ -1,4 +1,4 @@
-#include "DisplayDriver.h"
+#include "displayDriver.h"
 
 #ifdef AMOLED_DISPLAY
 

src/drivers/displays/display.h
@@ -1,7 +1,7 @@
 #ifndef DISPLAY_H
 #define DISPLAY_H
 
-#include "DisplayDriver.h"
+#include "displayDriver.h"
 
 extern DisplayDriver *currentDisplayDriver;
 

src/drivers/displays/dongleDisplayDriver.cpp
@@ -1,4 +1,4 @@
-#include "DisplayDriver.h"
+#include "displayDriver.h"
 
 #ifdef DONGLE_DISPLAY
 

src/drivers/displays/esp23_2432s028r.cpp
@@ -1,4 +1,4 @@
-#include "DisplayDriver.h"
+#include "displayDriver.h"
 
 #ifdef ESP32_2432S028R
 

src/drivers/displays/noDisplayDriver.cpp
@@ -1,4 +1,4 @@
-#include "DisplayDriver.h"
+#include "displayDriver.h"
 
 #ifdef NO_DISPLAY
 

src/drivers/displays/tDisplayDriver.cpp
@@ -1,4 +1,4 @@
-#include "DisplayDriver.h"
+#include "displayDriver.h"
 
 #ifdef T_DISPLAY
 

src/drivers/displays/tDisplayV1Driver.cpp
@@ -1,4 +1,4 @@
-#include "DisplayDriver.h"
+#include "displayDriver.h"
 
 #ifdef V1_DISPLAY
 
@@ -96,7 +96,6 @@ void tDisplay_MinerScreen(unsigned long mElapsed)
 
   // Push prepared background to screen
   background.pushSprite(0, 0);
-  background.deleteSprite();
 }
 
 void tDisplay_ClockScreen(unsigned long mElapsed)
@@ -135,7 +134,6 @@ void tDisplay_ClockScreen(unsigned long mElapsed)
 
   // Push prepared background to screen
   background.pushSprite(0, 0);
-  background.deleteSprite();
 }
 
 void tDisplay_GlobalHashScreen(unsigned long mElapsed)
@@ -195,7 +193,6 @@ void tDisplay_GlobalHashScreen(unsigned long mElapsed)
 
   // Push prepared background to screen
   background.pushSprite(0, 0);
-  background.deleteSprite();
 }
 
 void tDisplay_LoadingScreen(void)

src/drivers/displays/t_qtDisplayDriver.cpp
@@ -1,4 +1,4 @@
-#include "DisplayDriver.h"
+#include "displayDriver.h"
 
 #ifdef T_QT_DISPLAY
 