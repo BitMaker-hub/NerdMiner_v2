d4e3a31aaa6d4ee064777cf443b77bf84f103c07
p43lz3r <16374477+paelzer@users.noreply.github.com>
2024-01-21 18:35:13 +0100

    Update esp322432s028r.h

--- src/drivers/devices/esp322432s028r.h
+++ src/drivers/devices/esp322432s028r.h
@@ -1,12 +1,6 @@
 #ifndef _ESP32_2432S028R
 #define _ESP32_2432S028R
 
-// Defines for the SD card slot ESP32_2432S028R
-#define SDSPI_CS    5
-#define SDSPI_CLK   18
-#define SDSPI_MOSI  23
-#define SDSPI_MISO  19
-
 #define PIN_BUTTON_1 0
 //#define PIN_BUTTON_2 22  // Not used
 #define PIN_ENABLE5V 21  // Not used
