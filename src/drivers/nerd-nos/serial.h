93cc2a1a88995b9d46061e4cda9169c8e971c0cf
shufps <shufps80@gmail.com>
2024-09-14 10:55:34 +0200

    uses non-blocking read for asic response

--- src/drivers/nerd-nos/serial.h
+++ src/drivers/nerd-nos/serial.h
@@ -4,8 +4,8 @@
 
 int SERIAL_send(uint8_t *, int, bool);
 void SERIAL_init(void);
-void SERIAL_debug_rx(void);
 int16_t SERIAL_rx(uint8_t *, uint16_t, uint16_t);
+int16_t SERIAL_rx_non_blocking(uint8_t *buf, uint16_t size);
 void SERIAL_clear_buffer(void);
 void SERIAL_set_baud(int baud);
 
