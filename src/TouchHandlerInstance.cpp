a81423574ff2551792295892ea556929b11112cc
cosmicpsyop <cosmicpsyop@posteo.net>
2026-06-17 01:38:26 -0700

    feature(jc3248w535): remove src/TouchHandlerInstance.cpp

--- src/TouchHandlerInstance.cpp
+++
@@ -1,8 +0,0 @@
-#include "TouchHandler.h"
-#include "drivers/devices/device.h"
-
-// Provide a single global TouchHandler instance for the build.
-// The pins and configuration are defined in the device headers.
-#ifdef TOUCH_ENABLE
-TouchHandler touchHandler(ETOUCH_CS, TOUCH_IRQ, SPI);
-#endif
